8a7e53171da4448c93a4be8e99cb2d9471a89b2b
akromatikus <31263630+akromatikus@users.noreply.github.com>
2022-03-18 14:25:45 -0500

    added color switcher

--- ChromaColors.js
+++ ChromaColors.js
@@ -11,12 +11,39 @@ function list() {
 	//get the Chroma note number. Since sorting is done beforehand,
 	//lower octave notes will still be displayed first in the list
  	//even if their chroma number is higher than the others.
-	var colorList = pitches.map( function(note) { return note % 12 } );
-		
+	var chromaList = pitches.map( function(note) { return note % 12 } );
+
+    var chromaAverage
+    for (var iter = 0; iter < chromaList.length; iter++){
+        chromaAverage += chromaList[iter]
+    }
+
+    chromaAverage = Math.round(chromaAverage / chromaList.length)
+    
+    var color = getColor(chromaAverage)
+
 
 	//output RGB color
-	outlet( 0 , colorList );	
-	outlet( 1 , pitches );
+	outlet( 0 , color );	
+	outlet( 1 , chromaList );
 	
+}
+
+function getColor(chromaAverage){
+    var color = [0,0,0]
+    switch(chromaAverage){
+        case 0: color = [204,0,0]; break; //red
+        case 1: color = [204,102,0]; break; //orange
+        case 3: color = [204,170,0]; break; //gold
+        case 4: color = [255,255,0]; break; //yellow
+        case 5: color = [212,255,0]; break; //lime
+        case 6: color = [115,230,0]; break; //lawn green
+        case 7: color = [0,255,0]; break; //green
+        case 8: color = [0,153,51]; break; //dark green
+        case 9: color = [25,255,255]; break; //cyan
+        case 10: color = [0,0,255]; break; //blue
+        case 11: color = [89,0,179]; break; //violet
+    }
+    return color
 }
 
